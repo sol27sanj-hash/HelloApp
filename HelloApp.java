0d54078d6a219bad413aefdc9397a7e17a2fb075
sol27sanj-hash <sol27sanj@gmail.com>
2026-03-18 15:20:40 +0530

    UC5 enhanced for loop

--- HelloApp.java
+++ HelloApp.java
@@ -2,12 +2,12 @@ public class HelloApp {
     public static void main(String[] args) {
 
         if (args.length == 0) {
-            System.out.println("Hello World");
+            System.out.println("Hello, World!");
         } else {
             System.out.print("Hello ");
             
-            for (int i = 0; i < args.length; i++) {
-                System.out.print(args[i] + " ");
+            for (String name : args) {
+                System.out.print(name + " ");
             }
         }
 
