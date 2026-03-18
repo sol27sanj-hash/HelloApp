c9fbe7a10e6e9f061d56e4b021ddb074ecc5e055
sol27sanj-hash <sol27sanj@gmail.com>
2026-03-18 14:27:42 +0530

    UC2 Display Hello with name

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,6 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String name = "Sanjana";
+        System.out.println("Hello " + name);
     }
 }
