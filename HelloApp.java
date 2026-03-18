fbbba6d9c4b23fbfe37c17c13d56c851d76e0866
sol27sanj-hash <sol27sanj@gmail.com>
2026-03-18 15:14:57 +0530

    UC4 display multiple names

--- HelloApp.java
+++ HelloApp.java
@@ -1,10 +1,14 @@
 public class HelloApp {
     public static void main(String[] args) {
 
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
-        } else {
+        if (args.length == 0) {
             System.out.println("Hello World");
+        } else {
+            System.out.print("Hello ");
+            
+            for (int i = 0; i < args.length; i++) {
+                System.out.print(args[i] + " ");
+            }
         }
 
     }
