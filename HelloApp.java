63b6f587255090c33a8e6f7a98485ea9b375ec29
sol27sanj-hash <sol27sanj@gmail.com>
2026-03-18 15:00:28 +0530

    third usecase

--- HelloApp.java
+++ HelloApp.java
@@ -1,6 +1,11 @@
 public class HelloApp {
     public static void main(String[] args) {
-        String name = "Sanjana";
-        System.out.println("Hello " + name);
+
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Hello World");
+        }
+
     }
 }
